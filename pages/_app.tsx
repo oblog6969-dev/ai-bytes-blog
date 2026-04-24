b297faa7376da590342db7c51e5dd473ea86ec43
oblog6969-dev <oblog6969@gmail.com>
2026-04-24 20:23:54 +0300

    feat: add Google Analytics (G-GLGDFZ534C)
    Integrate Google Analytics tracking with route change handling.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,59 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
-/**
- * Main App component - wraps all pages
- * Imports global styles including Tailwind CSS
- */
+const GA_MEASUREMENT_ID = 'G-GLGDFZ534C';
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+    dataLayer: any[];
+  }
+}
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (typeof window.gtag !== 'undefined') {
+        window.gtag('config', GA_MEASUREMENT_ID, {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
